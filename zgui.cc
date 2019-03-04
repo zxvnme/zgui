7dd7b5f4857dd6f21c228d58583a7dc477e89f92
Patryk <wiotqmail@gmail.com>
2019-03-04 23:16:50 +0100

    Update zgui.cc

--- zgui.cc
+++ zgui.cc
@@ -544,9 +544,8 @@ void zgui::slider_float(std::string name, float min, float max, float* value)
 }
 void zgui::multi_combobox(std::string name, std::vector<multi_item> items)
 {
-	const int control_width = 70;
-	const int control_height = 20;
-
+	const auto control_width = 100;
+	const auto control_height = 20;
 
 	vec2 cursor_pos = this->pop_cursor_pos();
 	vec2 draw_pos{ context.window.position.x + cursor_pos.x, context.window.position.y + cursor_pos.y };
@@ -569,9 +568,11 @@ void zgui::multi_combobox(std::string name, std::vector<multi_item> items)
 	functions.draw_filled_rect(draw_pos.x, draw_pos.y, control_width, control_height, this->global_colors.control_outline);
 	functions.draw_filled_rect(draw_pos.x + 1, draw_pos.y + 1, control_width - 2, control_height - 2, this->global_colors.control_idle);
 
-	functions.draw_text(draw_pos.x + 60, draw_pos.y + 4, this->global_colors.color_text, context.window.font, false, "+");
+	functions.draw_text(draw_pos.x + control_width - 10, draw_pos.y + 4, this->global_colors.color_text, context.window.font, false, "+");
+
 	std::string value_str;
 	int text_wide, text_tall;
+
 	for (auto &item_t : items) {
 		if (*item_t.value) {
 			if (value_str.length() > 0)
@@ -580,18 +581,19 @@ void zgui::multi_combobox(std::string name, std::vector<multi_item> items)
 			value_str += item_t.name;
 		}
 	}
+
 	std::wstring text = std::wstring(value_str.begin(), value_str.end());
 	functions.get_text_size(context.window.font, text.c_str(), text_wide, text_tall);
-	if(text_wide > 60)
+	if (text_wide > control_width - 18)
 	{
-		value_str.resize(7);
-		value_str += ",...";
+		value_str.resize(control_width / 10);
+		value_str += " ...";
 	}
-	if(!value_str.length())
+	if (!value_str.length())
 		value_str += "None";
-	
+
 	functions.draw_text(draw_pos.x + 4, draw_pos.y + 4, this->global_colors.color_text, context.window.font, false, value_str.c_str());
-	
+
 	if (context.window.blocking == std::hash<std::string>()(name))
 	{
 		this->push_cursor_pos(vec2{ cursor_pos.x + control_width + ITEM_SPACING, cursor_pos.y });
@@ -620,10 +622,9 @@ void zgui::multi_combobox(std::string name, std::vector<multi_item> items)
 			}
 
 			functions.draw_filled_rect(draw_pos.x, draw_pos.y + 19 * i, control_width, control_height, this->global_colors.control_outline);
-			functions.draw_filled_rect(draw_pos.x + 1, draw_pos.y + (19 * i) + 1, control_width - 2, control_height - 2, *items[i - 1].value ? this->global_colors.control_active_or_clicked : this->global_colors.control_idle);
-			functions.draw_filled_rect(draw_pos.x + 1, draw_pos.y + (19 * i) + 1, control_width - 2, control_height - 2, hovered ? this->global_colors.color_combo_bg : this->global_colors.control_idle);
-			
-			functions.draw_text(draw_pos.x + 3, draw_pos.y + (control_height - 1) * i + 4, this->global_colors.color_text, context.window.font, false, i*items[i - 1].name.c_str());
+			functions.draw_filled_rect(draw_pos.x + 1, draw_pos.y + (19 * i) + 1, control_width - 2, control_height - 2, *items[i - 1].value || hovered ? this->global_colors.control_active_or_clicked : this->global_colors.control_idle);
+
+			functions.draw_text(draw_pos.x + 3, draw_pos.y + (control_height - 1) * i + 4, this->global_colors.color_text, context.window.font, false, items[i - 1].name.c_str());
 		}
 	}
 }
