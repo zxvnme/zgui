3fe7e91a061f7d4d8f4fca94df251d78f428f23d
Igor <zxvnme@protonmail.com>
2019-07-12 06:19:42 +0200

    added Visual Studio solution

---
+++ src/dllmain.cc
@@ -0,0 +1,15 @@
+﻿#include <Windows.h>
+
+BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
